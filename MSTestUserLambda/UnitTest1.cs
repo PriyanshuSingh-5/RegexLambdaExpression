9fab48b5f7f8d723e039aa9367ecd1c5fd9316d2
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-30 19:41:22 +0530

    [Priyanshu]modified validate using multiple emails

--- MSTestUserLambda/UnitTest1.cs
+++ MSTestUserLambda/UnitTest1.cs
@@ -7,17 +7,76 @@ namespace MSTestUserLambda
     [TestClass]
     public class UnitTest1
     {
+
+        RegexPattern pattern = new RegexPattern();
+
         [TestMethod]
 
         public void GivenUserFistName_WhenValidate_ShouldReturnresult()
         {
-            RegexPattern pattern = new RegexPattern();//Create Object
+            
             bool expected = true;
             //Act
             bool output = pattern.ValidateFirstName("Priyanshu");
             //Assert
             Assert.AreEqual(expected, output);
         }
+        [TestMethod]
+        public void GivenLastNameShouldReturnHappy()
+        {
+            bool expected = true;
+            bool output = pattern.ValidateLastName("Singh");
+            Assert.AreEqual(expected, output);
+        }
+
+       
+        [TestMethod]
+        public void GivenValidEmailIdShouldReturnHappy()
+        {
+            //Arrange
+            bool expected = true;
+            //Act
+            bool output = pattern.ValidateEmail("abc@gmail.com");
+            // Assert
+            Assert.AreEqual(expected, output);
+        }
+
+
+        
+        [TestMethod]
+        public void GivenValidMobileShouldReturnHappy()
+        {
+            bool expected = true;
+            bool output = pattern.ValidatePhoneNumber("91 9146293697");
+            Assert.AreEqual(expected, output);
+        }
+
+        /// <summary>
+        /// Given Valid Password should return true
+        /// </summary>
+        [TestMethod]
+        public void GivenValidPasswordShouldReturnHappy()
+        {
+            bool expected = true;
+            bool output = pattern.ValidatePassword("Shaloo@05");
+            Assert.AreEqual(expected, output);
+        }
+
+        [DataTestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void GivenValidEmailListShouldReturnsTrue(string email)
+        {
+            bool expected = true;
+            var result = pattern.ValidateEmailList(email);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
-
