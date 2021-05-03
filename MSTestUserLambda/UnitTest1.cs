8a104d75273b7f58d9b7ba18901a81acfe6f6626
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-03 13:10:48 +0530

    [Priyanshu] modified user details

--- MSTestUserLambda/UnitTest1.cs
+++ MSTestUserLambda/UnitTest1.cs
@@ -4,108 +4,90 @@ using UserRegisLambdaExpression;
 
 namespace MSTestUserLambda
 {
+
     [TestClass]
     public class Test
     {
-        //valid first name
-        [TestMethod]
-        public void GivenFirstNameShouldReturnHappy()
-        {
-            try
-            {
-                //Act
-                bool output = pattern.ValidateFirstName("Priyanshu");
-            }
+        private readonly RegexPattern User;
+    public Test()
+    {
+        User = new RegexPattern(); //create object
+    }
+    
+        //validate first name
+    [TestMethod]
+    public void validateFirstName()
+    {
+        var Result = User.ValidateFirstName("Priyanshu");
+        Assert.AreEqual(true, Result);
+    }
 
-            catch (RegistrationException e)
-            
-            {
-                //Assert
-                Assert.AreEqual("Invalid First Name,Name should be first letter capital", e.Message);
-            }
-        }
+    [TestMethod]
+    public void validateLastName()
+    {
+        var Result = User.ValidateLastName("Singh");
+        Assert.AreEqual(true, Result);
+    }
 
-       //valid last name 
-        [TestMethod]
-        public void GivenLastNameShouldReturnHappy()
-        {
-            try
-            {
-                bool output = pattern.ValidateLastName("Singh");
-            }
-            catch (RegistrationException e)
-            {
-                Assert.AreEqual("Invalid Last Name,Name should be first letter capital", e.Message);
-            }
-        }
+    [TestMethod]
+    public void Validate_Email()
+    {
+        var Result = User.ValidateEmail("priya12@gmail.com");
+        Assert.AreEqual(true, Result);
+    }
 
-      // ValidateEmails
-        [TestMethod]
-        public void GivenValidEmailIdShouldReturnHappy()
-        {
+    [TestMethod]
+    public void Validate_PhonNum()
+    {
+        var Result = User.ValidatePhoneNumber("91 9865432666");
+        Assert.AreEqual(true, Result);
+    }
 
-            try
-            {
-                //Act
-                bool output = pattern.ValidateEmail("abc@gmail.com");
-            }
-            catch (RegistrationException e)
-            {
-                // Assert
-                Assert.AreEqual("Invalid Email", e.Message);
-            }
-        }
+    [TestMethod]
+    public void Validate_Password()
+    {
+        var Result = User.ValidatePassword("shiva@12");
+        Assert.AreEqual(true, Result);
 
+    }
 
-        //valid mobile number or not
-        [TestMethod]
-        public void GivenValidMobileShouldReturnHappy()
-        {
-            try
-            {
-                bool output = pattern.ValidateMobileNo("91 9146293697");
-            }
-            catch (RegistrationException e)
-            {
-                Assert.AreEqual("Invalid Mobile number,number should be predefined format", e.Message);
-            }
-        }
+    /// <summary>
+    /// Validation using lambda expressions
+    /// </summary>
+    [TestMethod]
+    public void FirstNameUsingLambdaExpression()
+    {
+        var Result = User.ValidateFirstName("Shaloo");
+        Assert.AreEqual(true, Result);
+    }
 
-        //valid password or not
-        public void GivenValidPasswordShouldReturnHappy()
-        {
-            try
-            {
-                bool output = pattern.ValidatePassword("Shaloo@123");
-            }
-            catch (RegistrationException e)
-            {
-                Assert.AreEqual("Invalid Password,Password should be in format", e.Message);
-            }
+    [TestMethod]
+    public void LastNameUsingLambdaExpression()
+    {
+        var Result = User.ValidateLastName("Singh");
+        Assert.AreEqual(true, Result);
+    }
 
-        }
+    [TestMethod]
+    public void EmailUsingLambdaExpression()
+    {
+        var Result = User.ValidateEmail("shaloo@gmail.com");
+        Assert.AreEqual(true, Result);
+    }
 
-       //Email
-        [DataTestMethod]
-        [DataRow("abc@yahoo.com")]
-        [DataRow("abc-100@yahoo.com")]
-        [DataRow("abc.100@yahoo.com")]
-        [DataRow("abc111@abc.com")]
-        [DataRow("abc-100@abc.net")]
-        [DataRow("abc.100@abc.com.au")]
-        [DataRow("abc@1.com")]
-        [DataRow("abc@gmail.com.com")]
-        [DataRow("abc+100@gmail.com")]
-        public void GivenValidEmailListShouldReturnsTrue(string email)
-        {
-            try
-            {
-                var result = pattern.validateEmailList(email);
-            }
-            catch (RegistrationException e)
-            {
-                Assert.AreEqual("Invalid Email.Email should be in format", e.Message);
-            }
-        }
+    [TestMethod]
+    public void MobileNumberUsingLabdaExpression()
+    {
+        var Result = User.ValidatePhoneNumber("91 9975833272");
+        Assert.AreEqual(true, Result);
+    }
+
+    [TestMethod]
+    public void PasswordUingLambdaExpression()
+    {
+        var Result = User.ValidatePassword("Ranu@123");
+        Assert.AreEqual(true, Result);
     }
 }
+}
+
--- UserRegisLambdaExpression/RegexPattern.cs
+++ UserRegisLambdaExpression/RegexPattern.cs
@@ -9,11 +9,11 @@ namespace UserRegisLambdaExpression
 {
     public class RegexPattern
     {
-        public const string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";// pattren for firstname
-        public const string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";//pattren for lastname
-        public const string Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";//// pattren for  Email
-        public const string PhoneNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";// pattren for  number 
-        public const string Password = "^(?=.*[A-Z])(?=.*\\d)[\\S]{8,}$";// pattren for  password
+        public const string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public const string PhoneNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
+        public const string Password = "^(?=.*[A-Z])(?=.*\\d)[\\S]{8,}$";
 
         //Using Lambda Expressions
         public bool ValidateFirstName(string firstName)
@@ -86,4 +86,11 @@ namespace UserRegisLambdaExpression
 
         }
     }
+
+    //Using Lambda Expressions
+    public bool ValidateFirstName(string firstName) => (Regex.IsMatch(firstName, FirstName));
+    public bool ValidateLastName(string lastName) => (Regex.IsMatch(lastName, LastName));
+    public bool ValidateEmail(string email) => (Regex.IsMatch(email, Email));
+    public bool ValidateMobile(string mobNumber) => (Regex.IsMatch(mobNumber, PhoneNumber));
+    public bool ValidatePassword(string password) => (Regex.IsMatch(password, Password));
 }
